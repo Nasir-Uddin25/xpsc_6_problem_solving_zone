32acf544f729b4c56bcf393934a77787601fba13
Nasir Uddin <nasiruddin3954@gmail.com>
2025-04-08 12:36:31 +0600

    week_1

--- Week_2/A_Make_it_White.cpp
+++ Week_2/A_Make_it_White.cpp
@@ -5,6 +5,27 @@ int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    
+
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        string s;
+        cin >> s;
+
+        int firstpos = -1, lastpos = -1;
+        firstpos = s.find('B');
+        lastpos = s.rfind('B');
+
+        // cout << firstpos <<" " << lastpos;
+        int ans = lastpos - firstpos + 1;
+        cout << ans;
+        cout << endl;
+    }
+
     return 0;
 }
