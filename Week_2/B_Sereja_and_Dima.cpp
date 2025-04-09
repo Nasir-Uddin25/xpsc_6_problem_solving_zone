9efe152df1ce74f8277d9e12d8177d6d4b4a8f11
Nasir Uddin <nasiruddin3954@gmail.com>
2025-04-09 17:59:35 +0600

    Week_2

--- Week_2/B_Sereja_and_Dima.cpp
+++ Week_2/B_Sereja_and_Dima.cpp
@@ -5,11 +5,42 @@ int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
+
     int n;
     cin >> n;
 
     vector<int> v(n);
     for (int i = 0; i < n; i++)
         cin >> v[i];
+
+    int SerejaScore = 0, DimaScore = 0, moves = 1;
+
+    while (!v.empty())
+    {
+        int left = v.front() , right = v.back(), mx;
+        mx = max(left, right);
+
+        if (moves % 2 != 0)
+        {
+            SerejaScore += mx;
+        }
+         else
+        {
+            DimaScore += mx;
+        }
+
+        if (mx == left)
+        {
+            v.erase(v.begin());
+        }
+        else
+        {
+            v.pop_back();
+        }
+
+        moves++;
+    }
+
+    cout << SerejaScore << " " << DimaScore;
     return 0;
 }
