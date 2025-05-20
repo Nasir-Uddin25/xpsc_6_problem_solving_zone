d283a68f407a0ecc2a25c053d4be61f28913751f
Nasir Uddin <nasiruddin3954@gmail.com>
2025-05-20 17:54:52 +0600

    xpsc

---
+++ Week_7/D_NASA.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int a = 13, b = 27;
+    int ans = (a ^ b);
+    cout << ans << endl;
+
+    int x = 27, y = 22;
+    int ans1 = (x ^ y);
+    cout << ans1 << endl;
+
+    int c = 13, d = 22;
+    int ans2 = (c ^ d);
+    cout << ans2 << endl;
+    return 0;
+}
